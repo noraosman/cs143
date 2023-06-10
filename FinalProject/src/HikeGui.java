296dcabda00025e60e86d37f4b4fc4feeb6bac36
Kyle Williams <kylewilliams342@gmail.com>
2023-06-09 21:49:45 -0700

    Updated GUI with working List.

--- FinalProject/src/HikeGui.java
+++ FinalProject/src/HikeGui.java
@@ -1,9 +1,7 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HikeGui extends HikeMain2{
@@ -20,20 +18,49 @@ public class HikeGui extends HikeMain2{
     private JRadioButton difficultRadioButton;
     private JTextField longitudeText;
     private JTextField latitudeText;
-    private JLabel logo;
-
+    private JPanel listPanel;
+    private JButton moreInfoButton;
+    private JList list1;
+    private HikeNode[] finalList;
+    private JPopupMenu popupMenu;
     public HikeGui() {
         continueButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // only use for actions
-                System.out.println("hi");
+                String region;
+                if (easternRadioButton.isSelected()) {
+                    region = "Eastern";
+                } else {
+                    region = "Western";
+                }
+
+                String difficulty;
+                if (easyRadioButton.isSelected()) {
+                    difficulty = "Easy";
+                } else {
+                    difficulty = "Difficult";
+                }
+
+                double latitude = Double.parseDouble(latitudeText.getText());
+                double longitude = Double.parseDouble(longitudeText.getText());
+
+                try {
+                    finalList = createList(region, difficulty, latitude, longitude);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                fillJList();
+
+
+
 
             }
         });
     }
 
     public void showGui() throws IOException {
+
+
         JFrame frame = new JFrame("Take A Hike!");
         frame.setContentPane(new HikeGui().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,9 +71,12 @@ public class HikeGui extends HikeMain2{
 
     }
 
-    private void createUIComponents() throws IOException {
-        BufferedImage myPic = ImageIO.read(new File("logo.png"));
-        this.logo = new JLabel(new ImageIcon(myPic));
-
+    private void fillJList() {
+        String[] arrOfStrings = new String[5];
+        for (int i = 0; i < arrOfStrings.length; i++) {
+            arrOfStrings[i] = finalList[i].getData().name();
+        }
+        list1.setListData(arrOfStrings);
     }
+
 }
--- FinalProject/src/HikeMain2.java
+++ FinalProject/src/HikeMain2.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class HikeMain2 {
     public static final String EASTERN_WASHINGTON = "EasternHikes.csv";
     public static final String WESTERN_WASHINGTON = "WesternHikes.csv";
@@ -6,7 +12,51 @@ public class HikeMain2 {
     public static void main(String[] args) throws Exception{
         HikeGui GUI = new HikeGui();
         GUI.showGui();
-        
+
     }
+
+    public static HikeNode[] createList(String region, String difficulty, double latitude, double longitude) throws FileNotFoundException {
+        Scanner in;
+        SuggestionList suggestionList;
+        List<String[]> hikes = new ArrayList<>();
+
+        if (region.equals("Eastern")) {
+            in = new Scanner(new File(EASTERN_WASHINGTON));
+        } else {
+            in = new Scanner(new File(WESTERN_WASHINGTON));
+        }
+
+        writeFile(in, hikes);
+
+        HikeList list = new HikeList(hikes, latitude, longitude);
+        System.out.println(list);
+
+        if (difficulty.equals("Easy")) {
+            suggestionList = new SuggestionList(list.getEasy(), latitude, longitude);
+        } else {
+            suggestionList = new SuggestionList(list.getDifficult(), latitude, longitude);
+        }
+
+        HikeNode[] finalHikeList = new HikeNode[5];
+        int count = 0;
+        for (double key : suggestionList.getMap().keySet()) {
+            finalHikeList[count] = suggestionList.getMap().get(key);
+            count++;
+        }
+
+        return finalHikeList;
+    }
+
+
+
+    public static void writeFile(Scanner in, List<String[]> list) {
+        while (in.hasNextLine()) {
+            String[] hike = in.nextLine().split(",");
+            list.add(hike);
+        }
+    }
+
+
+
 
 }
